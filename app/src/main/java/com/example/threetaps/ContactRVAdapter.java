33cb293d1a1ccaf1c03993deecff5fe3ba977281
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-01 13:44:13 +0900

    contact list showing

--- app/src/main/java/com/example/threetaps/ContactRVAdapter.java
+++ app/src/main/java/com/example/threetaps/ContactRVAdapter.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
 import java.util.ArrayList;
 
 class ContactRVAdapter extends RecyclerView.Adapter<ContactRVAdapter.ViewHolder>{
@@ -34,7 +37,37 @@ class ContactRVAdapter extends RecyclerView.Adapter<ContactRVAdapter.ViewHolder>
 
     @Override
     public void onBindViewHolder(@NonNull ContactRVAdapter.ViewHolder holder, int position) {
+        // getting data from array list in our modal.
+        ContactsModal modal = contactsModalArrayList.get(position);
+        // on below line we are setting data to our text view.
+        holder.contactTV.setText(modal.getUserName());
+        ColorGenerator generator = ColorGenerator.MATERIAL; // or use DEFAULT
+        // generate random color
+        int color = generator.getRandomColor();
 
+        // below text drawable is a circular.
+        TextDrawable drawable2 = TextDrawable.builder().beginConfig()
+                .width(100)  // width in px
+                .height(100) // height in px
+                .endConfig()
+                // as we are building a circular drawable
+                // we are calling a build round method.
+                // in that method we are passing our text and color.
+                .buildRound(modal.getUserName().substring(0, 1), color);
+        // setting image to our image view on below line.
+        holder.contactIV.setImageDrawable(drawable2);
+        // on below line we are adding on click listener to our item of recycler view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are opening a new activity and passing data to it.
+                Intent i = new Intent(context, ContactDetailActivity.class);
+                i.putExtra("name", modal.getUserName());
+                i.putExtra("contact", modal.getContactNumber());
+                // on below line we are starting a new activity,
+                context.startActivity(i);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/threetaps/Tab1.java
+++ app/src/main/java/com/example/threetaps/Tab1.java
@@ -1,18 +1,192 @@
 package com.example.threetaps;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tab1 extends Fragment {
+
+    // to use the context here
+    private MainActivity mainActivity;
+    // creating variables for our array list, recycler view progress bar and adapter.
+    private ArrayList<ContactsModal> contactsModalArrayList;
+    private RecyclerView contactRV;
+    private ContactRVAdapter contactRVAdapter;
+    private ProgressBar loadingPB;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        // assign context here for later use
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_tab1, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // on below line we are initializing our variables.
+        contactsModalArrayList = new ArrayList<>();
+        contactRV = view.findViewById(R.id.RVContacts);
+        loadingPB = view.findViewById(R.id.PBLoading);
+
+        // calling method to prepare our recycler view.
+        prepareContactRV();
+
+        // calling a method to request permissions.
+        requestPermissions();
+
+    }
+
+    private void prepareContactRV() {
+        // in this method we are preparing our recycler view with adapter.
+        contactRVAdapter = new ContactRVAdapter(mainActivity, contactsModalArrayList);
+        // on below line we are setting layout manager.
+        contactRV.setLayoutManager(new LinearLayoutManager(mainActivity));
+        // on below line we are setting adapter to our recycler view.
+        contactRV.setAdapter(contactRVAdapter);
+    }
+
+    private void requestPermissions() {
+        // below line is use to request
+        // permission in the current activity.
+        Dexter.withContext(mainActivity)
+                // below line is use to request the number of
+                // permissions which are required in our app.
+                .withPermissions(android.Manifest.permission.READ_CONTACTS,
+                        android.Manifest.permission.CALL_PHONE)
+                // after adding permissions we are
+                // calling and with listener method.
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        // this method is called when all permissions are granted
+                        if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                            // do your work now
+                            getContacts();
+                            Toast.makeText(mainActivity, "All the permissions are granted..", Toast.LENGTH_SHORT).show();
+                        }
+                        // check for permanent denial of any permission
+                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
+                            // permission is denied permanently,
+                            // we will show user a dialog message.
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        // this method is called when user grants some
+                        // permission and denies some of them.
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).withErrorListener(new PermissionRequestErrorListener() {
+                    // this method is use to handle error
+                    // in runtime permissions
+                    @Override
+                    public void onError(DexterError error) {
+                        // we are displaying a toast message for error message.
+                        Toast.makeText(mainActivity, "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                // below line is use to run the permissions
+                // on same thread and to check the permissions
+                .onSameThread().check();
+    }
+
+    private void showSettingsDialog() {
+
+    }
+
+    @SuppressLint("Range")
+    private void getContacts() {
+        // this method is use to read contact from users device.
+        // on below line we are creating a string variables for
+        // our contact id and display name.
+        String contactId = "";
+        String displayName = "";
+        // on below line we are calling our content resolver for getting contacts
+        Cursor cursor = mainActivity.getContentResolver()
+                .query(ContactsContract.Contacts.CONTENT_URI, null, null, null,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+        // on below line we are checking the count for our cursor.
+        if (cursor.getCount() > 0) {
+            // if the count is greater than 0 then we are running a loop to move our cursor to next.
+            while (cursor.moveToNext()) {
+                // on below line we are getting the phone number.
+                // An indicator of whether this contact has at least one phone number.
+                // "1" if there is at least one phone number, "0" otherwise.
+                int hasPhoneNumber = Integer.parseInt(cursor
+                        .getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+                    // we are checking if the has phone number is > 0
+                    // on below line we are getting our contact id and user name for that contact
+                    contactId = cursor.getString(cursor
+                            .getColumnIndex(ContactsContract.Contacts._ID));
+                    displayName = cursor.getString(cursor
+                            .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    // on below line we are calling a content resolver and making a query
+                    Cursor phoneCursor = mainActivity.getContentResolver().query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{contactId},
+                            null);
+                    // on below line we are moving our cursor to next position.
+                    if (phoneCursor.moveToNext()) {
+                        // on below line we are getting the phone number for our users
+                        // and then adding the name along with phone number in array list.
+                        String phoneNumber = phoneCursor.getString(phoneCursor
+                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contactsModalArrayList.add(new ContactsModal(displayName, phoneNumber));
+                    }
+                    // on below line we are closing our phone cursor.
+                    phoneCursor.close();
+                }
+            }
+        }
+        // on below line we are closing our cursor.
+        cursor.close();
+        // on below line we are hiding our progress bar and notifying our adapter class.
+        loadingPB.setVisibility(View.GONE);
+        contactRVAdapter.notifyDataSetChanged();
+    }
+
 }
