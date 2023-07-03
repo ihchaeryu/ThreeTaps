3161e2b68ab2685edfcc34d0085a3b13336afaf8
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-03 13:42:21 +0900

    tab3 structure
    rebase

--- app/src/main/java/com/example/threetaps/Tab3.java
+++ app/src/main/java/com/example/threetaps/Tab3.java
@@ -1,18 +1,188 @@
 package com.example.threetaps;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tab3 extends Fragment {
+
+    // variables
+    // to use the context here
+    private MainActivity mainActivity;
+    private ArrayList<MusicModal> musicModalArrayList;
+    private RecyclerView musicRV;
+    private MusicRVAdapter musicRVAdapter;
+    private ProgressBar loadingPB;
+    // activity result launcher to solve deprecation
+    private ActivityResultLauncher<Intent> launcher;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        // assign context here for later use
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // activity result launcher to solve deprecation
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Handle the result here
+                    }
+                });
+
         return inflater.inflate(R.layout.fragment_tab3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // initialize variables
+        musicModalArrayList = new ArrayList<>();
+        musicRV = view.findViewById(R.id.RVMusic);
+        loadingPB = view.findViewById(R.id.PBLoading);
+
+        // calling method to prepare our recycler view.
+        prepareMusicRV();
+
+        // calling a method to request permissions.
+        requestPermissions();
+    }
+
+    private void prepareMusicRV() {
+        // prepare RV adapter
+        musicRVAdapter = new MusicRVAdapter(mainActivity, musicModalArrayList);
+        // set layout manager
+        musicRV.setLayoutManager(new LinearLayoutManager(mainActivity));
+        // set RV with adapter
+        musicRV.setAdapter(musicRVAdapter);
+    }
+
+    private void requestPermissions() {
+        // request permission via Dexter
+        Dexter.withContext(mainActivity)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        // this method is called when all permissions are granted
+                        if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                            // get all music list
+                            getMusic();
+                            Toast.makeText(mainActivity, "All the permissions are granted.", Toast.LENGTH_SHORT).show();
+                        }
+                        // check for permanent denial of any permission
+                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
+                            // permission is denied permanently,
+                            // we will show user a dialog message.
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        // this method is called when user grants some
+                        // permission and denies some of them.
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).withErrorListener(new PermissionRequestErrorListener() {
+                    // this method is use to handle error
+                    // in runtime permissions
+                    @Override
+                    public void onError(DexterError error) {
+                        // we are displaying a toast message for error message.
+                        Toast.makeText(mainActivity, "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                // below line is use to run the permissions
+                // on same thread and to check the permissions
+                .onSameThread().check();
+    }
+
+    private void getMusic() {
+        // this method is doing the work! get the music data from user's device
+
+    }
+
+    private void showSettingsDialog() {
+        // dialog message for when a permission is permanently denied
+
+        // we are displaying an alert dialog for permissions
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+
+        // below line is the title
+        // for our alert dialog.
+        builder.setTitle("Need Permissions");
+
+        // below line is our message for our dialog
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // this method is called on click on positive
+                // button and on clicking shit button we
+                // are redirecting our user from our app to the
+                // settings page of our app.
+                dialog.cancel();
+                // below is the intent from which we
+                // are redirecting our user.
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", mainActivity.getPackageName(), null);
+                intent.setData(uri);
+//                startActivityForResult(intent, 101);
+                launcher.launch(intent);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // this method is called when
+                // user click on negative button.
+                dialog.cancel();
+            }
+        });
+        // below line is used
+        // to display our dialog
+        builder.show();
     }
 }
