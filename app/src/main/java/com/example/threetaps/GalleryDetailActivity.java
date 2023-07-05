268b14f56beab76029199f64440e2e79781562d1
fbre0717 <koh2040@naver.com>
2023-07-05 15:10:05 +0900

    230705

--- app/src/main/java/com/example/threetaps/GalleryDetailActivity.java
+++ app/src/main/java/com/example/threetaps/GalleryDetailActivity.java
@@ -6,18 +6,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.chrisbanes.photoview.PhotoView;
+
 import java.util.ArrayList;
 
-public class GalleryDetailActivity extends AppCompatActivity implements View.OnTouchListener {
+public class GalleryDetailActivity extends AppCompatActivity {
     private String uriString;
     private int position;
     private ArrayList<String> galleryModalArrayList;
-    private ImageView imageView;
-
+    private PhotoView photoView;
+    private Button btnPrevious, btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,57 +31,44 @@ public class GalleryDetailActivity extends AppCompatActivity implements View.OnT
         galleryModalArrayList  = intent.getStringArrayListExtra("stringArrayList_key");
 
         if (uriString != null) {
-            imageView = findViewById(R.id.detailImageView);
-            imageView.setImageURI(Uri.parse(uriString));
-            imageView.setOnTouchListener(this);
+            photoView = findViewById(R.id.detailPhotoView);
+            photoView.setImageURI(Uri.parse(uriString));
+            photoView.setMaximumScale(10.0f);
+            photoView.setMinimumScale(1.0f);
             Toast.makeText(this,  String.valueOf(position), Toast.LENGTH_SHORT).show();
+
+            btnPrevious = findViewById(R.id.btnPrevious);
+            btnNext = findViewById(R.id.btnNext);
+            setButtonClickListeners();
         } else {
             Log.e("GalleryDetailActivity", "Uri is null");
         }
     }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (v.getId() == R.id.detailImageView) {
-            float screenWidth = v.getWidth();
-            float touchX = event.getX();
-            boolean touchInLeft20Percent = touchX <= screenWidth * 0.2;
-            boolean touchInRight20Percent = touchX >= screenWidth * 0.8;
-
-            if (touchInLeft20Percent) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        // 좌측 20% 영역 터치 시작
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        // 좌측 20% 영역 터치 이후 작동
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        // 좌측 20% 영역 터치 종료
-                        if(position > 0){
-                            imageView.setImageURI(Uri.parse(galleryModalArrayList.get(--position)));
-                            Toast.makeText(this,  String.valueOf(position), Toast.LENGTH_SHORT).show();
-                        }
-                        break;
-                }
-            } else if (touchInRight20Percent) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        // 우측 20% 영역 터치 시작
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        // 우측 20% 영역 터치 이후 작동
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        // 우측 20% 영역 터치 종료
-                        if(position < galleryModalArrayList.size() - 1){
-                            imageView.setImageURI(Uri.parse(galleryModalArrayList.get(++position)));
-                            Toast.makeText(this,  String.valueOf(position), Toast.LENGTH_SHORT).show();
-                        }
-                        break;
-                }
+    private void setButtonClickListeners() {
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPreviousImage();
             }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNextImage();
+            }
+        });
+    }
+    private void showPreviousImage() {
+        if(position > 0){
+            photoView.setImageURI(Uri.parse(galleryModalArrayList.get(--position)));
+            Toast.makeText(this,  String.valueOf(position), Toast.LENGTH_SHORT).show();
         }
-        return true;
+    }
+    private void showNextImage() {
+        if(position < galleryModalArrayList.size() - 1){
+            photoView.setImageURI(Uri.parse(galleryModalArrayList.get(++position)));
+            Toast.makeText(this,  String.valueOf(position), Toast.LENGTH_SHORT).show();
+        }
     }
 }
